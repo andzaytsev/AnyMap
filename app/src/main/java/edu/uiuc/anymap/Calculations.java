95de283a776fc08b2b6711afd35fb9df36e86276
temandr <brentwoodpav@gmail.com>
2014-09-14 00:25:00 -0500

    functions

--- app/src/main/java/edu/uiuc/anymap/Calculations.java
+++ app/src/main/java/edu/uiuc/anymap/Calculations.java
@@ -8,14 +8,13 @@ public class Calculations {
     private static Point p2;
     private static Point p3;
 
+    //Index of current point
+    public static int i;
+
     public void setPoint(int i, Point x){
         if(i == 1) p1 = x;
         else if(i==2) p2 = x;
         else if(i==3) p3 = x;
-    }
-
-    public int checkBlank(){
-        return p1.xGPS == -1 ? (p2.xGPS == -1 ? (p3.xGPS == -1 ? 0 : 3) : 2) : 1;
     }
 
 
